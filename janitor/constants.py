62db4600ea2f73110d9cfe0c5e033e2b75c0a65c
lavit797 <lavittyagi2004@gmail.com>
2026-05-21 08:10:09 +0000

    adding constants and requirement.txt

---
+++ janitor/constants.py
@@ -0,0 +1,3 @@
+EBS_GP3_PER_GB_MONTH = 0.08      # $0.08/GB/month
+EIP_IDLE_PER_MONTH = 3.65        # $3.65/month unassociated EIP
+EC2_T3_MICRO_PER_MONTH = 7.59    # $7.59/month on-demand us-east-1
